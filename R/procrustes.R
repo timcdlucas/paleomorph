249661ea276ae1ce80dbc8ef59322092cacaeb10
Tim Lucas <timcdlucas@gmail.com>
2016-01-21 16:58:45 +0000

    halfway through pctstep.

--- R/procrustes.R
+++ R/procrustes.R
@@ -98,15 +98,17 @@ unzapa <- function(a, b){
 
 
 
-#' pcrstep
+#' Rotate all shapes until optimally aligned.
 #'
-#' Given an M x N x 3 array,
+#' Given an M x N x 3 array (M = no of specimens, N = no of landmarks.) 
+#'   this will find the optimal rotation for all shapes so that they are as
+#'   aligned as possible.
 #'
 #'@param a An M x N x 3 array. M = no of specimens, N = no of landmarks.
 #'@param maxiter Maximum number of iterations to attempt
-#'@param tolerance How close 
+#'@param tolerance Difference between two iterations that will cause the search to stop. 
 #'
-#'@return An M x N x 3 array 
+#'@return An M x N x 3 array of aligned shapes
 
 
 pcrstep <- function(a, maxiter = 1000, tolerance = 10e-7){
@@ -161,71 +163,118 @@ pcrstep <- function(a, maxiter = 1000, tolerance = 10e-7){
 
 
 
-(*
- * returns new "a"
- *)
-pcrstep[a_,m_,n_] := Module[ {na, na2, count, ta, i, j, k, c, r},
 
-    na = zapa[a,m,n];	(* no missing data in sight now *)
+#' Translate all shapes until optimally aligned.
+#'
+#' Given an M x N x 3 array (M = no of specimens, N = no of landmarks.) 
+#'   this will find the optimal translation for all shapes so that they are as
+#'   aligned as possible.
+#'
+#'@param a An M x N x 3 array. M = no of specimens, N = no of landmarks.
+#'
+#'@return An M x N x 3 array of aligned shapes
 
-    done = False;
-    For[count = 1, count <= 1000, count++,
 
-	na2 = na;   (* save *)
-	If[Mod[count, 50] == 0, Print["rstep: reached ", count, "th iteration"]];
+pctstep <- function(a){
 
-	For[i = 2, i <= m, i++,
 
-	    (*
-	     * Compute ta, the temporary matrix that will be used
-	     * in this iteration
-	     *)
-	    ta = Table[0.0, {j,1,n}, {k,1,3}];
-	    For[j = 1, j <= m, j++,
-		If[ i != j, ta += na[[j]] ];
+
+#     * The linear algebra here is a little tricky.
+#     * We have M equations in M unknowns, but one equation is redundant
+#     * and the solution space is invariant under a common translation.
+#     *
+#     * For now, we deal with this by throwing away the first equation
+#     * and forcing the first unknown to equal 0.
+#     * A more robust solution might be: retain all M equations and solve by
+#     * least squares, and force the sum of the unknowns to 0.
+
+  # m numer of specimens, n number of landmarks
+  m <- dim(a)[1]
+  n <- dim(a)[2]
+
+  # inialise objects
+  c <- matrix(0, nrow = m - 1, ncol = m - 1)
+  bx <- rep(0, m - 1) 
+  by <- rep(0, m - 1) 
+  bz <- rep(0, m - 1) 
+
+
+  for(i in seq(2, m)){
+    for(j in seq(m)){
+      if (i != j) {
+        for(k in seq(n)){
+          if (all(!is.na(a[i, k, ])) && all(!is.na(a[j, k, ]))) {
+            c[i - 1, i - 1] <- c[i - 1, i - 1] + 1
+            if (j > 1) {
+              c[i - 1, j - 1] <- c[i - 1, j - 1] - 1
+            }
+
+            bx[i - 1] <- bx[i - 1] + a[i, k, 1] - a[j, k, 1]
+            by[i - 1] <- by[i - 1] + a[i, k, 2] - a[j, k, 2]
+            bz[i - 1] <- bz[i - 1] + a[i, k, 3] - a[j, k, 3]
+          }          
+        }
+      }
+    }
+  }
+
+
+
+
+}
+
+
+pctstep[a_,m_,n_] := Module[ {i, j, k, c, bx, by, bz, v, na},
+
+    (*
+     * The linear algebra here is a little tricky.
+     * We have M equations in M unknowns, but one equation is redundant
+     * and the solution space is invariant under a common translation.
+     *
+     * For now, we deal with this by throwing away the first equation
+     * and forcing the first unknown to equal 0.
+     * A more robust solution might be: retain all M equations and solve by
+     * least squares, and force the sum of the unknowns to 0.
+     *)
+
+    c  = Table[0.0, {i,1,m-1}, {j,1,m-1}];
+    bx = Table[0.0, {i,1,m-1}];
+    by = Table[0.0, {i,1,m-1}];
+    bz = Table[0.0, {i,1,m-1}];
+
+    For[i = 2, i <= m, i++,
+	For[j = 1, j <= m, j++,
+	    If[i == j, Continue[]];
+	    For[k = 1, k <= n, k++,
+		If[StringQ[a[[i,k]]] || StringQ[a[[j,k]]], Continue[]];
+		c[[i-1,i-1]] += 1.0;
+		If[j > 1, c[[i-1,j-1]] -= 1.0];
+		bx[[i-1]] += a[[i,k,1]] - a[[j,k,1]];
+		by[[i-1]] += a[[i,k,2]] - a[[j,k,2]];
+		bz[[i-1]] += a[[i,k,3]] - a[[j,k,3]];
 	    ];
-
-	    (*
-	     * Compute na[[i]]^T (ta)
-	     * The rotation which best approximates this matrix will be
-	     * applied to na[[i]]
-	     *)
-	    c = Transpose[na[[i]]] . ta;
-	    r = rpdecompose[c][[1]];
-	    na[[i]] = lrotate[na[[i]], r];
 	];
-
-	If[deltaa[na,na2,m,n] < 10^(-7), Break[]];
     ];
 
-    na = unzapa[na,a,m,n];
-    Print["rstep: score=", scorea[na,m,n], " delta=", deltaa[a,na,m,n], " iterations=", count];
+    (* FIXME: emit warning here if matrix is badly conditioned *)
+
+    (*
+     * FIXME: this could be made more efficient by using
+     * LUDecomposition[] and LUBackSubstitution[] instead of LinearSolve[]
+     *)
+    v = Transpose[ {LinearSolve[c, bx],
+		    LinearSolve[c, by],
+		    LinearSolve[c, bz]
+		 }];
+
+    na = Table[
+	    If[i==1, a[[1]], lshift[a[[i]], -v[[i-1]]]],
+	    {i,1,m}
+    ];
+    Print["tstep: score=", scorea[na,m,n], " delta=", deltaa[a,na,m,n]];
     Return[na];
 ];
 
-
-
-
-#pctstep <- function(a, m, n){
-#  for(i in seq(2, m)){
-#    for(j in seq(m)){
-#      if (i != j) {
-#        for(k in seq(n)){
-#          if (sum(a[i, k, ]) != 0 && sum(a[j, k, ]) != 0) {
-#            c[i - 1, i - 1] <- c[i - 1, i - 1] + 1
-#            if (j > 1) {
-#              c[i - 1, j - 1] <- c[i - 1, j - 1] - 1
-#            }
-
-#            bx[i - 1] <- bx[i - 1] + a[i, k, 1] - a[j, k, 1]
-#            by[i - 1] <- by[i - 1] + a[i, k, 2] - a[j, k, 2]
-#            bz[i - 1] <- bz[i - 1] + a[i, k, 3] - a[j, k, 3]
-#          }          
-#        }
-#      }
-#    }
-#  }
-#}
 
 
 
