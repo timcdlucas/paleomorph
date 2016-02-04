d24fa7a8e1c1f4d23a1dc46a3370f84e82133118
Tim Lucas <timcdlucas@gmail.com>
2016-02-04 12:16:38 +0000

    1D cvm added.

--- R/cvm.R
+++ R/cvm.R
@@ -33,7 +33,7 @@
 
 dotcvm <- function(M){
   # Calculate covariance between each pairs of columns.
-  N <- outer(1:dim(M)[2], 1:dim(M)[2], dotcvmentryVector, M = M)
+  N <- outer(1:dim(M)[2], 1:dim(M)[2], dotcvmentry, M = M)
   e <- min(eigen(N)$values)
   if(e < 0) warning(paste('CVM has negative eigenvalue', e))
   return(N)
@@ -41,16 +41,19 @@ dotcvm <- function(M){
 
 
 
-dotcvm[M_] := Module[ {i,j,N,e},
-   N = Table[dotcvmentry[M,i,j], {i,1,Dimensions[M][[2]]}, {j,1,Dimensions[M][[2]]}];
-   e = Min[Eigenvalues[N]];
-   If[e < 0, Print["warning: CVM has negative eigenvalue ", e];];
-   N
-];
+#' Calculate 3D covariance between two landmarks across specimens 
+#' 
+#'
+#'@param M An M x N x 3 array. M = no of specimens, N = no of landmarks.
+#'@col1 Integer of first column 
+#'@col2 Integer of second column 
+#'
+#'@return 3D covariance matrix
+
 
 
 # Check columns have enough data and then calculate covariance between columns
-dotcvmentry <- function(M, col1, col2){
+dotcvmentry <- Vectorize(function(M, col1, col2){
   n <- 0
   s1 <- c(0, 0, 0)
   s2 <- c(0, 0, 0)
@@ -79,9 +82,57 @@ dotcvmentry <- function(M, col1, col2){
   }
 
   return(p/(n - 1))
+}, vectorize.args=list('col1', 'col2'))
+
+
+
+
+
+
+
+cvmentry <- Vectorize(function(M, col1, col2){
+  n <- 0
+  s1 <- 0
+  s2 <- 0
+
+  # For each specimen
+  for(i in 1:dim(M)[1]){
+    if(!anyNA(M[, c(col1, col2)])){
+      n <- n + 1
+      s1 <- s1 + M[i, col1]
+      s2 <- s2 + M[i, col1]
+    }
+  }
+
+  if(n <= 1) stop(paste("There is too much missing data  covary columns", col1, "and", col2))
+
+  s1 <- s1/n
+  s2 <- s2/n
+
+  p <- 0
+
+  # for each specimen
+  for(i in 1:dim(M)[1]){
+    if(!anyNA(M[, c(col1, col2)])){
+      p <- p + (M[i, col1] - s1) * (M[i, col2] - s2)
+    }
+  }
+
+  return(p/(n - 1))
+
+}, vectorize.args=list('col1', 'col2'))
+
+
+
+cvm <- function(M){
+  # Calculate covariance between each pairs of columns.
+  N <- outer(1:dim(M)[2], 1:dim(M)[2], cvmentry, M = M)
+  e <- min(eigen(N)$values)
+  if(e < 0) warning(paste('CVM has negative eigenvalue', e))
+  return(N)
 }
 
-dotcvmentryVector <- Vectorize(dotcvmentry, vectorize.args=list('col1', 'col2'))
+
 
 
 
